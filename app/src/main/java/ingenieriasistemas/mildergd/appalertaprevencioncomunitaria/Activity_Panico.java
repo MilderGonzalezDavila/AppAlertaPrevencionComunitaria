0614a10029884e37997ce17d9cc5f5a44e0ba793
Arturo <jesusarturo965@gmail.com>
2026-02-08 19:38:50 -0500

    Agrege Iconos, colores, y temas | avance de actividad_panico

---
+++ app/src/main/java/ingenieriasistemas/mildergd/appalertaprevencioncomunitaria/Activity_Panico.java
@@ -0,0 +1,19 @@
+package ingenieriasistemas.mildergd.appalertaprevencioncomunitaria;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Activity_Panico extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_panico);
+    }
+}
