6b376a9b3f6833bad886a297f527637527190bc4
MilderGonzalez <mgonzalezd23_1@unc.edu.pe>
2026-02-11 08:15:56 -0500

    Estandarize los nombres de las ventanas, ly_iniciar_sesion.xml, ly_recuperar_clave.xml y ly_registrarse.xml. Para que todas los layouts tengas el mismo patron de escritura.

--- app/src/main/java/ingenieriasistemas/mildergd/appalertaprevencioncomunitaria/ActivityIniciarSesion.java
+++ app/src/main/java/ingenieriasistemas/mildergd/appalertaprevencioncomunitaria/ActivityIniciarSesion.java
@@ -2,18 +2,14 @@ package ingenieriasistemas.mildergd.appalertaprevencioncomunitaria;
 
 import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class ActivityIniciarSesion extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.ly_iniciar_sesion);
+        setContentView(R.layout.activity_iniciar_sesion);
 
     }
 }
